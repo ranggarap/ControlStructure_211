3e84269a805b9de8a281f7bf64261d6dd2c3b7f7
ranggarap <fadhilahrangga26@gmail.com>
2025-10-07 11:09:34 +0700

    revisi

---
+++ prak3.py
@@ -0,0 +1,7 @@
+n = int (input("masukan nilai n untuk batas deret fibonanci:"))
+
+a, b = o, 1
+
+print ("deret fibonanci hingga", n, ":")
+while a <= n:
+    a, b, = a + b
---
+++ prak4.py
@@ -0,0 +1,5 @@
+n = int (input("masukan nilai n untuk bilangan ganjil: "))
+
+print ("bilangan ganjil hingga ", n, ":")
+for i in range ( 1, n + 1, 2):
+    print (1, end = " ")
